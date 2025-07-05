23883e872ff64325b97bb61f0d829f3384d28b6e
Diego Brito <dibbrito@gmail.com>
2025-07-04 21:38:33 -0300

    feat: Define Car and Engine models with necessary fields and JSON tags

--- models/engine.go
+++ models/engine.go
@@ -1,1 +1,12 @@
 package models
+
+import (
+	"github.com/google/uuid"
+)
+
+type Engine struct {
+	EngineID      uuid.UUID `json:"engine_id"`
+	Displacement  int64     `json:"displacement"`
+	NoOfCylinders int       `json:"no_of_cylinders"`
+	CarRange      int64     `json:"car_range"`
+}
