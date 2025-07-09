a7b0d13f0b7f074252ba590577048752ec05f59c
Diego Brito <dibbrito@gmail.com>
2025-07-09 16:28:20 -0300

    feat: Implement CarService with CRUD operations for car management

--- service/car/car.go
+++ service/car/car.go
@@ -1,1 +1,62 @@
 package car
+
+import (
+	"context"
+
+	"github.com/diegobbrito/car-zone/models"
+	"github.com/diegobbrito/car-zone/store"
+)
+
+type CarService struct {
+	store store.CarStoreInterface
+}
+
+func NewCarService(store store.CarStoreInterface) *CarService {
+	return &CarService{store: store}
+}
+
+func (s *CarService) GetCarById(ctx context.Context, id string) (*models.Car, error) {
+	car, err := s.store.GetCarById(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &car, nil
+}
+
+func (s *CarService) GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error) {
+	cars, err := s.store.GetCarByBrand(ctx, brand, isEngine)
+	if err != nil {
+		return nil, err
+	}
+	return cars, nil
+}
+
+func (s *CarService) CreateCar(ctx context.Context, carRequest *models.CarRequest) (*models.Car, error) {
+	if err := models.ValidateRequest(*carRequest); err != nil {
+		return nil, err
+	}
+	createdCar, err := s.store.CreateCar(ctx, carRequest)
+	if err != nil {
+		return nil, err
+	}
+	return &createdCar, nil
+}
+
+func (s *CarService) UpdateCar(ctx context.Context, id string, carRequest *models.CarRequest) (*models.Car, error) {
+	if err := models.ValidateRequest(*carRequest); err != nil {
+		return nil, err
+	}
+	updatedCar, err := s.store.UpdateCar(ctx, id, carRequest)
+	if err != nil {
+		return nil, err
+	}
+	return &updatedCar, nil
+}
+
+func (s *CarService) DeleteCar(ctx context.Context, id string) (*models.Car, error) {
+	deletedCar, err := s.store.DeleteCar(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	return &deletedCar, nil
+}
