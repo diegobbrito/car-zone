a1aefe1ffd2d4afc5a453a66409f496a2575662c
Diego Brito <dibbrito@gmail.com>
2025-07-11 20:15:19 -0300

    feat: Define CarServiceInterface with CRUD operations for car management

--- service/interface.go
+++ service/interface.go
@@ -1,1 +1,15 @@
 package service
+
+import (
+	"context"
+
+	"github.com/diegobbrito/car-zone/models"
+)
+
+type CarServiceInterface interface {
+	GetCarById(ctx context.Context, id string) (models.Car, error)
+	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
+	CreateCar(ctx context.Context, carRequest *models.CarRequest) (models.Car, error)
+	UpdateCar(ctx context.Context, id string, carRequest *models.CarRequest) (models.Car, error)
+	DeleteCar(ctx context.Context, id string) (models.Car, error)
+}
