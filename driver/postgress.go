f2f9a3301f115f2de18b281606d4478ff61cd89c
Diego Brito <dibbrito@gmail.com>
2025-07-13 13:46:16 -0300

    feat: Implement database connection initialization and management functions

--- driver/postgress.go
+++ driver/postgress.go
@@ -1,1 +1,45 @@
 package driver
+
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+	"time"
+)
+
+var db *sql.DB
+
+func InitDB() {
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_PORT"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	fmt.Println("Waiting for database connection...")
+	time.Sleep(5 * time.Second)
+
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatalf("Error connecting to the database: %v", err)
+	}
+	err = db.Ping()
+	if err != nil {
+		log.Fatalf("Error pinging the database: %v", err)
+	}
+
+	log.Println("Database connection established successfully")
+}
+
+func GetDB() *sql.DB {
+	return db
+}
+
+func CloseDB() {
+	if err := db.Close(); err != nil {
+		log.Printf("Error closing the database connection: %v", err)
+	}
+}
