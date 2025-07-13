c88674de85824e5e610d189786ca10ba4becfe7b
Diego Brito <dibbrito@gmail.com>
2025-07-13 13:46:11 -0300

    fix: Update return types in CarServiceInterface and EngineServiceInterface to use pointers

--- service/interface.go
+++ service/interface.go
@@ -7,16 +7,16 @@ import (
 )
 
 type CarServiceInterface interface {
-	GetCarById(ctx context.Context, id string) (models.Car, error)
+	GetCarById(ctx context.Context, id string) (*models.Car, error)
 	GetCarByBrand(ctx context.Context, brand string, isEngine bool) ([]models.Car, error)
-	CreateCar(ctx context.Context, carRequest *models.CarRequest) (models.Car, error)
-	UpdateCar(ctx context.Context, id string, carRequest *models.CarRequest) (models.Car, error)
-	DeleteCar(ctx context.Context, id string) (models.Car, error)
+	CreateCar(ctx context.Context, carRequest *models.CarRequest) (*models.Car, error)
+	UpdateCar(ctx context.Context, id string, carRequest *models.CarRequest) (*models.Car, error)
+	DeleteCar(ctx context.Context, id string) (*models.Car, error)
 }
 
 type EngineServiceInterface interface {
-	GetEngineByID(ctx context.Context, id string) (models.Engine, error)
-	CreateEngine(ctx context.Context, engineRequest *models.EngineRequest) (models.Engine, error)
-	UpdateEngine(ctx context.Context, id string, engineRequest *models.EngineRequest) (models.Engine, error)
-	DeleteEngine(ctx context.Context, id string) (models.Engine, error)
+	GetEngineByID(ctx context.Context, id string) (*models.Engine, error)
+	CreateEngine(ctx context.Context, engineRequest *models.EngineRequest) (*models.Engine, error)
+	UpdateEngine(ctx context.Context, id string, engineRequest *models.EngineRequest) (*models.Engine, error)
+	DeleteEngine(ctx context.Context, id string) (*models.Engine, error)
 }
